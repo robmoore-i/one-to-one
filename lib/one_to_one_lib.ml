47fe903ff5261f0f348fae0018f4829240a714e7
romo <robmoore121@gmail.com>
2020-08-06 23:30:27 +0800

    Learn a bit more OCaml.

--- lib/one_to_one_lib.ml
+++ lib/one_to_one_lib.ml
@@ -1,2 +1,11 @@
 let average a b =
   (a +. b) /. 2.0;;
+
+let square_plus_one a =
+  let a_squared = a * a in
+  a_squared + 1;;
+
+let cube_plus_one a =
+  let accumulator = ref 1 in
+  (accumulator := !accumulator + (a * a * a));
+  !accumulator;;
--- test/one_to_one_test.ml
+++ test/one_to_one_test.ml
@@ -1,12 +1,20 @@
-open One_to_one_lib;;
 open OUnit2;;
+module Oto = One_to_one_lib;;
 
 let test_average _ =
-  assert_equal 2.5 (average 2.0 3.0);;
+  assert_equal 2.5 (Oto.average 2.0 3.0);;
+
+let test_square_plus_one _ =
+  assert_equal 5 (Oto.square_plus_one 2);;
+
+let test_cube_plus_one _ =
+  assert_equal 9 (Oto.cube_plus_one 2);;
 
 let suite =
   "AverageTest" >::: [
-    "test_average" >:: test_average
+    "test_average" >:: test_average;
+    "test_square_plus_one" >:: test_square_plus_one;
+    "test_cube_plus_one" >:: test_cube_plus_one
   ];;
 
 run_test_tt_main suite
