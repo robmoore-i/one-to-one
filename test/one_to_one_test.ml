5e574b379931c7389926220c34b2528a6f320326
romo <robmoore121@gmail.com>
2020-08-06 18:42:16 +0800

    Got some tests running with ounit2

--- test/one_to_one_test.ml
+++ test/one_to_one_test.ml
@@ -1,1 +1,12 @@
-print_endline "Testing!"
+open One_to_one_lib;;
+open OUnit2;;
+
+let test_average _ =
+  assert_equal 2.5 (average 2.0 3.0);;
+
+let suite =
+  "AverageTest" >::: [
+    "test_average" >:: test_average
+  ];;
+
+run_test_tt_main suite
--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 eval $(opam config env)
 opam install httpaf
+opam install ounit2
 dune build one_to_one_test.exe
 dune exec ./one_to_one_test.exe
